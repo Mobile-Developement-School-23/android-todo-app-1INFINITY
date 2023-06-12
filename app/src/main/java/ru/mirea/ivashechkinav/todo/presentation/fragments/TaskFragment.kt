a27404cadf0dee4ef0580e725bc9807939463fd9
1INFINITY <praecantatio@mail.ru>
2023-06-13 02:56:21 +0300

    added return to main page

--- app/src/main/java/ru/mirea/ivashechkinav/todo/presentation/fragments/TaskFragment.kt
+++ app/src/main/java/ru/mirea/ivashechkinav/todo/presentation/fragments/TaskFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import ru.mirea.ivashechkinav.todo.data.repository.TodoItemsRepositoryImpl
 import ru.mirea.ivashechkinav.todo.databinding.FragmentTaskBinding
@@ -21,11 +22,20 @@ class TaskFragment : Fragment() {
     ): View? {
         binding = FragmentTaskBinding.inflate(inflater, container, false)
         loadArgs()
+        initButtons()
         return binding.root
     }
-    private fun loadArgs(){
+
+    private fun loadArgs() {
         val todoItem = repository.getItemById(args.taskId ?: return) ?: return
         binding.edTodoItemText.setText(todoItem.text)
         binding.swDeadline.isChecked = todoItem.deadlineTimestamp != null
+    }
+
+    private fun initButtons() {
+        binding.btnSave.setOnClickListener { findNavController().popBackStack() }
+        binding.imDelete.setOnClickListener { findNavController().popBackStack() }
+        binding.imCancel.setOnClickListener { findNavController().popBackStack() }
+
     }
 }
