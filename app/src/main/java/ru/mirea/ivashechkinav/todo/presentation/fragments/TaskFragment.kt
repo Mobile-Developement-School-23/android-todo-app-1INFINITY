665fa7cd6b8ef8e0e16bc96278a44c4a962aa17e
1INFINITY <praecantatio@mail.ru>
2023-06-15 00:39:07 +0300

    added popup menu

--- app/src/main/java/ru/mirea/ivashechkinav/todo/presentation/fragments/TaskFragment.kt
+++ app/src/main/java/ru/mirea/ivashechkinav/todo/presentation/fragments/TaskFragment.kt
@@ -1,12 +1,20 @@
 package ru.mirea.ivashechkinav.todo.presentation.fragments
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import ru.mirea.ivashechkinav.todo.R
+import ru.mirea.ivashechkinav.todo.data.models.Importance
+import ru.mirea.ivashechkinav.todo.data.models.TodoItem
 import ru.mirea.ivashechkinav.todo.data.repository.TodoItemsRepositoryImpl
 import ru.mirea.ivashechkinav.todo.databinding.FragmentTaskBinding
 
@@ -23,6 +31,7 @@ class TaskFragment : Fragment() {
         binding = FragmentTaskBinding.inflate(inflater, container, false)
         loadArgs()
         initButtons()
+        initPopUpMenu()
         return binding.root
     }
 
@@ -36,6 +45,33 @@ class TaskFragment : Fragment() {
         binding.btnSave.setOnClickListener { findNavController().popBackStack() }
         binding.imDelete.setOnClickListener { findNavController().popBackStack() }
         binding.imCancel.setOnClickListener { findNavController().popBackStack() }
+    }
 
+    private fun initPopUpMenu() {
+        val popupMenu = PopupMenu(requireContext(), binding.vPopupMenuAnchor)
+        popupMenu.inflate(R.menu.popup_menu)
+        val highElement: MenuItem = popupMenu.menu.getItem(2)
+        val s = SpannableString(highElement.title)
+        s.setSpan(ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.color_light_red)), 0, s.length, 0)
+        highElement.title = s
+
+        binding.flImportanceMenu.setOnClickListener {popupMenu.show()}
+
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            val newImportance = when (menuItem.itemId) {
+                R.id.menu_item_none -> {
+                    Importance.LOW
+                }
+                R.id.menu_item_low -> {
+                    Importance.COMMON
+                }
+                R.id.menu_item_high -> {
+                    Importance.HIGH
+                }
+                else -> Importance.LOW
+            }
+            //Set importance to item
+            true
+        }
     }
 }
